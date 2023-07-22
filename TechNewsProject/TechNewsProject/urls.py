80b1207451d0a92c36360e91f9fbfd664879ff89
Ndhlovu1 <tinomudaishendhlovu@gmail.com>
2023-07-22 04:08:55 +0200

    Preparing for launching to the cloud

--- TechNewsProject/TechNewsProject/urls.py
+++ TechNewsProject/TechNewsProject/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/',include('blog_app.urls',namespace='blog')),
+    path('',include('blog_app.urls',namespace='blog')),
     
 ]
